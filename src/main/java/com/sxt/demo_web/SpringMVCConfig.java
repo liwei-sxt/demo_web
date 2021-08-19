19da12c9c9b7ebe5de5423ec3f37c8416abbfaa1
lixiaowei <742619423@qq.com>
2021-08-19 16:04:34 +0800

    修改了SpringMVCConfig文件！

--- src/main/java/com/sxt/demo_web/SpringMVCConfig.java
+++ src/main/java/com/sxt/demo_web/SpringMVCConfig.java
@@ -3,6 +3,7 @@ package com.sxt.demo_web;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
@@ -14,5 +15,10 @@ public class SpringMVCConfig implements WebMvcConfigurer {
     @Override
     public void configureViewResolvers(ViewResolverRegistry registry) {
         registry.jsp("/",".jsp");
+    }
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/demo").setViewName("hello");
     }
 }
